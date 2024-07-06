40a74a9e97fac9ca5748c543a62a576daf91cb32
wisdomMichaels <ijarwisdom@gmail.com>
2024-07-06 16:53:27 +0100

    added tailwind  file

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 // BMI FORMULA = WEIGHT(KG) / HIGHT (MS)
 
-function bmiCalculator(weight, height) {
-    let bmi = weight/ Math.pow(height, 2);
-    return Math.round(bmi);
-    
-}
 
-console.log(bmiCalculator(65, 1.8));
+const weight = document.getElementById (weight);
+const height = document.getElementById(height);
+const result = document.getElementById(result);
+const btn = document.getElementById(btn);
+
+Form.addEventlistner("click"(), =>{
+    
+})
+
+// function bmiCalculator(weight, height) {
+//     let bmi = weight/ Math.pow(height, 2);
+//     return Math.round(bmi);
+    
+// }
+
+// console.log(bmiCalculator(65, 1.8));
