8c15ca24021cf7f7438ae7db5edb411b799afeb9
wisdomMichaels <ijarwisdom@gmail.com>
2024-07-07 10:37:52 +0100

    added js function

---
+++ README.md
@@ -0,0 +1,30 @@
+BMI Calculator
+Description
+A simple BMI (Body Mass Index) calculator built using HTML, CSS, and JavaScript. This calculator takes in user input for weight and height, and calculates the BMI based on the provided values.
+
+Features
+User-friendly interface with input fields for weight and height
+Calculates BMI based on user input
+Displays the calculated BMI result
+Includes a BMI weight guide for reference
+Getting Started
+Prerequisites
+A modern web browser (e.g. Google Chrome, Mozilla Firefox)
+No installation required...
+
+Technologies Used
+HTML5
+CSS3 (with Tailwind CSS framework)
+JavaScript (for calculation and DOM manipulation)
+Assets
+assets/pexels-823sl-2294363.jpg: a background image used in the project
+Script
+script.js: contains the JavaScript code for calculating the BMI and updating the result display
+License
+This project is licensed under the MIT License. See the LICENSE file for details.
+
+Author
+[Wisdom Michael]
+
+Version
+1.0.0
--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 // BMI FORMULA = WEIGHT(KG) / HIGHT (MS)
 
+const form = document.querySelector('form');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results');
 
-const weight = document.getElementById (weight);
-const height = document.getElementById(height);
-const result = document.getElementById(result);
-const btn = document.getElementById(btn);
-
-Form.addEventlistner("click"(), =>{
-    
-})
-
+    if (height === '' || height < 0 || isNaN(height)) {
+        results.innerHTML = `Please give a valid weight and height`;
+    } else if (weight === '' || weight < 0 || isNaN(weight)) {
+        results.innerHTML = `Please give a valid weight and height`;
+    }else {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(1);
+        results.innerHTML = `${bmi}`;
+    }
+});
 // function bmiCalculator(weight, height) {
 //     let bmi = weight/ Math.pow(height, 2);
 //     return Math.round(bmi);
